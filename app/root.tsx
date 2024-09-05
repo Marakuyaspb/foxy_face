2f96af8a6dd6f747498ae6897a8bfc6847067903
Marakuyaspb <marakuyaspb@gmail.com>
2024-09-06 01:05:44 +0300

    one select fiels works

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import "./style.css";
+
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
